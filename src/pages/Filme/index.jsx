8825e112faa8900d82cd59267738f41166f32c4c
Rafaaelsz <rafaeltperaldo@gmail.com>
2024-11-07 14:32:07 -0300

    v2

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,7 +1,58 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from "../../services/api";
+
 function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "65316834fc77b1769d085f1d96222dba",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("Filme não encontrado.");
+        });
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("Componente foi desmontado.");
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="file-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Página detalhes do filme</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+
+      <strong>Avaliação: {filme.vote_avarage}</strong>
     </div>
   );
 }
